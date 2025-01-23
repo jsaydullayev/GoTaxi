f0a2c3a802f2807688471ae2be0b8c798812e382
joo <saydullayev_jahongir@mail.ru>
2025-01-23 18:11:27 +0500

    new code

--- GoTaxi.Api/Constants/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GoTaxi.Data.Constants
-{
-    public static class Role
-    {
-        public const string AdminRole = "admin";
-        public const string ClientRole = "client";
-        public const string OwnerRole = "driver";
-    }
-}
--- GoTaxi.Api/Program.cs
+++ GoTaxi.Api/Program.cs
@@ -1,17 +1,73 @@
 using GoTaxi.Api.Repositories;
+using GoTaxi.Common.Jwt;
 using GoTaxi.Data.Contracts;
 using GoTaxi.Data.GoTaxiDbContext;
+using GoTaxi.Data.Repositories;
 using GoTaxi.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<JwtSettings>>().Value);
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.AddSecurityDefinition("Yorvorjon", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+        {
+            Description = "JWT Bearer. : \"Authorization: Yorvorjon {token} \"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Yorvorjon"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    var jwtParam = builder.Configuration.GetSection("JwtParameters").Get<JwtSettings>();
+    var key = Encoding.UTF32.GetBytes(jwtParam.Key);
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidIssuer = jwtParam.Issuer,
+        ValidAudience = jwtParam.Audience,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+    };
+});
+
+
+
 builder.Services.AddDbContext<TaxiDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("TaxiDb"));
@@ -20,6 +76,8 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 builder.Services.AddScoped<IDestinationRepository, DestinationRepository>();
 builder.Services.AddScoped<ICarInfoRepository, CarInfoRepository>();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+builder.Services.AddScoped<IUserCarOrdersRepository, UserCarOrderRepository>();
 
 var app = builder.Build();
 
